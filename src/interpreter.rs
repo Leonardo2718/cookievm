b21f9e95a7de33469dba519454a39ddeca1707ee
Leonardo Banderali <leonardo2718@protonmail.com>
2018-05-04 14:32:44 -0600

    Add interpreter tests

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -135,3 +135,320 @@ impl<'a> Thread<'a> {
         self.stack.pop().ok_or("Cannot pop value from stack: stack is empty.".to_string())
     }
 }
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use super::cookie::*;
+    use super::cookie::Instruction::*;
+
+    #[test]
+    fn pushc_test_1() {
+        let insts = vec![
+            PUSHC(Value::I32(3)),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::I32(3));
+    }
+
+    #[test]
+    fn pushc_test_2() {
+        let insts = vec![
+            PUSHC(Value::F32(3.14159)),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::F32(3.14159));
+    }
+
+    #[test]
+    fn pushc_test_3() {
+        let insts = vec![
+            PUSHC(Value::Bool(true)),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::Bool(true));
+    }
+
+    #[test]
+    fn pushc_test_4() {
+        let insts = vec![
+            PUSHC(Value::Char('w')),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::Char('w'));
+    }
+
+    #[test]
+    fn pushc_test_5() {
+        let insts = vec![
+            PUSHC(Value::IPtr(0x5)),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::IPtr(0x5));
+    }
+
+    #[test]
+    fn pushc_test_6() {
+        let insts = vec![
+            PUSHC(Value::SPtr(0x5)),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::SPtr(0x5));
+    }
+
+    #[test]
+    fn pushc_test_7() {
+        let insts = vec![
+            PUSHC(Value::Void),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::Void);
+    }
+
+    #[test]
+    fn pop_test_1() {
+        let insts = vec![
+            PUSHC(Value::I32(2)),
+            POP,
+        ];
+        let mut thread = Thread::new(&insts);
+        assert!(thread.exec().unwrap().is_none());
+    }
+
+    #[test]
+    fn pop_test_2() {
+        let insts = vec![
+            PUSHC(Value::F32(2.71828)),
+            POP,
+        ];
+        let mut thread = Thread::new(&insts);
+        assert!(thread.exec().unwrap().is_none());
+    }
+
+    #[test]
+    fn pop_test_3() {
+        let insts = vec![
+            PUSHC(Value::Bool(true)),
+            POP,
+        ];
+        let mut thread = Thread::new(&insts);
+        assert!(thread.exec().unwrap().is_none());
+    }
+
+    #[test]
+    fn pop_test_4() {
+        let insts = vec![
+            PUSHC(Value::Void),
+            POP,
+        ];
+        let mut thread = Thread::new(&insts);
+        assert!(thread.exec().unwrap().is_none());
+    }
+
+    #[test]
+    fn binop_test_1() {
+        let insts = vec![
+            PUSHC(Value::I32(1)),
+            PUSHC(Value::I32(2)),
+            STACK_BINARY(BOp::ADD),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::I32(3));
+    }
+
+    #[test]
+    fn binop_test_2() {
+        let insts = vec![
+            PUSHC(Value::IPtr(0x7ab)),
+            PUSHC(Value::I32(4)),
+            STACK_BINARY(BOp::SUB),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::IPtr(0x7a7));
+    }
+
+    #[test]
+    fn binop_test_3() {
+        let insts = vec![
+            PUSHC(Value::F32(4.0)),
+            PUSHC(Value::I32(4)),
+            STACK_BINARY(BOp::ADD),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert!(thread.exec().is_err());
+    }
+
+    #[test]
+    fn binop_test_4() {
+        let insts = vec![
+            PUSHC(Value::Void),
+            PUSHC(Value::I32(4)),
+            STACK_BINARY(BOp::MUL),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert!(thread.exec().is_err());
+    }
+
+    #[test]
+    fn uniop_test_1() {
+        let insts = vec![
+            PUSHC(Value::I32(3)),
+            STACK_UNARY(UOp::NEG),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::I32(-3));
+    }
+
+    #[test]
+    fn uniop_test_2() {
+        let insts = vec![
+            PUSHC(Value::F32(2.71828)),
+            STACK_UNARY(UOp::NEG),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::F32(-2.71828));
+    }
+
+    #[test]
+    fn uniop_test_3() {
+        let insts = vec![
+            PUSHC(Value::Bool(false)),
+            STACK_UNARY(UOp::NOT),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::Bool(true));
+    }
+
+    #[test]
+    fn uniop_test_4() {
+        let insts = vec![
+            PUSHC(Value::I32(0)),
+            STACK_UNARY(UOp::NOT),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::I32(-1));
+    }
+
+    #[test]
+    fn uniop_test_5() {
+        let insts = vec![
+            PUSHC(Value::Void),
+            STACK_UNARY(UOp::NOT),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert!(thread.exec().is_err());
+    }
+
+    #[test]
+    fn pushr_test_1() {
+        let insts = vec![
+            PUSHR(RegisterName::StackPointer),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::SPtr(0));
+    }
+
+    #[test]
+    fn pushr_test_2() {
+        let insts = vec![
+            PUSHC(Value::I32(3)),
+            PUSHC(Value::F32(3.14159)),
+            PUSHC(Value::Char('c')),
+            PUSHR(RegisterName::StackPointer),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::SPtr(3));
+    }
+
+    #[test]
+    fn pushr_test_3() {
+        let insts = vec![
+            PUSHC(Value::I32(3)),
+            PUSHC(Value::F32(3.14159)),
+            PUSHC(Value::Char('c')),
+            POP,
+            PUSHR(RegisterName::StackPointer),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::SPtr(2));
+    }
+
+    #[test]
+    fn pushr_test_4() {
+        let insts = vec![
+            PUSHR(RegisterName::ProgramCounter),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::IPtr(0));
+    }
+
+    #[test]
+    fn pushr_test_5() {
+        let insts = vec![
+            PUSHC(Value::I32(3)),
+            PUSHC(Value::F32(3.14159)),
+            PUSHC(Value::Char('c')),
+            PUSHR(RegisterName::ProgramCounter),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::IPtr(3));
+    }
+
+    #[test]
+    fn pushr_test_6() {
+        let insts = vec![
+            PUSHC(Value::I32(3)),
+            PUSHC(Value::F32(3.14159)),
+            PUSHC(Value::Char('c')),
+            POP,
+            PUSHR(RegisterName::ProgramCounter),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::IPtr(4));
+    }
+
+    #[test]
+    fn popr_test_1() {
+        let insts = vec![
+            PUSHC(Value::I32(1)),
+            PUSHC(Value::I32(2)),
+            PUSHC(Value::I32(3)),
+            PUSHC(Value::I32(4)),
+            PUSHC(Value::SPtr(2)),
+            POPR(RegisterName::StackPointer),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert_eq!(thread.exec().unwrap().unwrap(), Value::I32(2));
+    }
+
+    #[test]
+    fn popr_test_2() {
+        let insts = vec![
+            PUSHC(Value::I32(1)),
+            POPR(RegisterName::StackPointer),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert!(thread.exec().is_err());
+    }
+
+    #[test]
+    fn popr_test_3() {
+        let insts = vec![
+            PUSHC(Value::IPtr(0x0)),
+            POPR(RegisterName::StackPointer),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert!(thread.exec().is_err());
+    }
+
+    #[test]
+    fn popr_test_4() {
+        let insts = vec![
+            PUSHC(Value::SPtr(0x2)),
+            POPR(RegisterName::ProgramCounter),
+        ];
+        let mut thread = Thread::new(&insts);
+        assert!(thread.exec().is_err());
+    }
+}
